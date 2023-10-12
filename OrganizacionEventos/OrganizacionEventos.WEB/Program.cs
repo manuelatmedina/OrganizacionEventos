c8292577a9151500e5298f03edd8dce8fb19d680
Manuela Tenganàn Medina <manuelamedinat@hotmail.com>
2023-10-11 22:05:30 -0500

    Inserciòn Tab Eventos NavMenu - Incorporaciòn de SweetAlert

--- OrganizacionEventos/OrganizacionEventos.WEB/Program.cs
+++ OrganizacionEventos/OrganizacionEventos.WEB/Program.cs
@@ -1,3 +1,4 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OrganizacionEventos.WEB;
@@ -7,9 +8,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000/") });
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
